5def8b544f567546c5e47b482e4395e2b8586b45
Peter Zhokhov <peter.zhokhov@sentient.ai>
2017-05-23 09:57:08 -0700

    modified setup.py to get requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session='hack')
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
+
 
 setup(name='tfstudio',
       version = '0.0',
@@ -6,12 +14,7 @@ setup(name='tfstudio',
       long_description = 'TensorFlow model and data management tool',
       url = 'https://github.com/ilblackdragon/studio',
       packages = ['studio'],
-      install_requires = [
-        'tensorflow',
-        'apscheduler',
-        'flask',
-        'jinja2'
-        ],
+      install_requires = reqs,
       scripts=['studio/scripts/studio-runner', 'studio/scripts/studio'],
       include_package_data=True,
       zip_safe = False)
