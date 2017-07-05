27c3fbdb0b787a38e9f76d2ca99621775cde2a45
Peter Zhokhov <peter.zhokhov@sentient.ai>
2017-07-05 14:15:30 -0700

    added force-git to cloud_worker_test

--- studio/tests/cloud_worker_test.py
+++ studio/tests/cloud_worker_test.py
@@ -1,12 +1,14 @@
 import unittest
 import os
+import uuid
 
 from studio.gcloud_worker import GCloudWorkerManager
 from studio.ec2cloud_worker import EC2WorkerManager
 from local_worker_test import stubtest_worker
+from studio.model import get_db_provider, get_config
 
 
-@unittest.skipIf(True or
+@unittest.skipIf(
     'GOOGLE_APPLICATION_CREDENTIALS' not in os.environ.keys(),
     'GOOGLE_APPLICATION_CREDENTIALS environment ' +
     'variable not set, won'' be able to use google cloud')
@@ -16,19 +18,22 @@ class GCloudWorkerTest(unittest.TestCase):
         return GCloudWorkerManager(project)
 
     def test_worker(self):
-
+        experiment_name = 'test_gcloud_worker_' + str(uuid.uuid4())
         stubtest_worker(
             self,
-            experiment_name='test_cloud_worker',
-            runner_args=['--cloud=gcloud'],
+            experiment_name=experiment_name,
+            runner_args=['--cloud=gcloud', '--force-git'],
             config_name='test_config.yaml',
             test_script='tf_hello_world.py',
             script_args=['arg0'],
             expected_output='[ 2.  6.]',
         )
+        get_db_provider(get_config('test_config.yaml')) \
+                .delete_experiment(experiment_name)
 
 
-@unittest.skipIf(True or
+
+@unittest.skipIf(
     'GOOGLE_APPLICATION_CREDENTIALS' not in os.environ.keys(),
     'GOOGLE_APPLICATION_CREDENTIALS environment ' +
     'variable not set, won'' be able to use google cloud')
@@ -37,16 +42,19 @@ class EC2WorkerTest(unittest.TestCase):
         return EC2WorkerManager()
 
     def test_worker(self):
-
+        experiment_name = 'test_ec2_worker_' + str(uuid.uuid4())
         stubtest_worker(
             self,
-            experiment_name='test_cloud_worker',
+            experiment_name=experiment_name,
             runner_args=['--cloud=ec2', '--force-git', '--gpus=1'],
             config_name='test_config.yaml',
             test_script='tf_hello_world.py',
             script_args=['arg0'],
             expected_output='[ 2.  6.]',
         )
+
+        get_db_provider(get_config('test_config.yaml')) \
+                .delete_experiment(experiment_name)
 
 
 if __name__ == '__main__':
