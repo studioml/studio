d6ee03b1809d1f6cbb541429ba2896fd0c770d0b
Peter Zhokhov <peter.zhokhov@sentient.ai>
2017-07-27 18:47:42 -0700

    model pipes work in progress

--- studio/model_util.py
+++ studio/model_util.py
@@ -1,18 +1,207 @@
 import types
 try:
     import keras
+    from keras.preprocessing import image as image_prep
 except ImportError:
     keras = None
 
 import logging
 
-from Queue import Full,Empty, Queue
-from multiprocessing import Process
+from PIL import Image
+
+from Queue import Full,Empty
+from multiprocessing import Process, Queue
 from threading import Thread
 import numpy as np
+import itertools 
 
 
 logging.basicConfig()
+
+class BufferedPipe:
+    def __init__(self, 
+                 func=lambda x:x, 
+                 parent=None,
+                 q_in=None,
+                 q_out=None,
+                 num_workers=0,
+                 q_size=10,
+                 batch_size=1,
+                 filterf=lambda x:x is not None,
+                 batcher=lambda x:x):
+
+
+        self.func = func
+        self.parent = parent
+        self.num_workers = num_workers
+        self.filterf = filterf
+        self.batch_size = batch_size
+        self.batcher = batcher
+
+        if num_workers > 0:
+            self.q_size = q_size if q_size else 2*num_workers
+
+        self.q_out = q_out
+        self.q_in = q_in
+        self.q_size = q_size
+        
+        self.logger = logging.getLogger('BufferedPipe')
+        self.logger.setLevel(10)
+        self.timeout = 10
+        self.worker_frame = Thread
+        
+    def __call__(self, data_gen):
+        if self.parent:
+            data_gen = self.parent(data_gen) 
+
+        if self.num_workers == 0 and \
+           self.batch_size == 1 and \
+           self.q_in is None and \
+           self.q_out is None:
+            return (self._wrapped_func(x) for x in data_gen)
+
+        q_in = self.q_in
+        if q_in is None:
+            q_in = Queue(self.q_size)
+            Thread(target=_gen2q, 
+                   args=(data_gen, q_in), 
+                   ).start()
+
+        q_out = self.q_out 
+        if q_out is None:
+            q_out = Queue(self.q_size)
+
+        if self.batch_size == 1:
+            target = _q2q_single
+            kwargs = {
+                "func":self._wrapped_func, 
+                "queue_in":q_in, 
+                "queue_out":q_out,
+                "filterf":self.filterf,
+                "timeout":self.timeout}
+        else:
+            target = _q2q_batch
+            kwargs = {
+                "func":self._wrapped_func, 
+                "queue_in":q_in, 
+                "queue_out":q_out,
+                "filterf":self.filterf, 
+                "batch_size":self.batch_size,
+                "timeout":self.timeout}
+
+        if self.num_workers == 0:
+            self.worker_frame(target=target, kwargs=kwargs).start()
+        else:
+            for i in range(self.num_workers):
+                self.worker_frame(target=target, 
+                                  kwargs=kwargs).start()
+
+        if self.q_out is None:
+            return _q2gen(q_out, timeout=self.timeout)
+
+    def add(self, func, 
+            num_workers=None, 
+            batch_size=None, 
+            filterf=None, 
+            batcher=None):
+
+        if num_workers == 0:
+            g = self.func
+            self.func = lambda x: func(g(x))
+            self.batch_size = batch_size
+            self.batcher = batcher
+            return self
+
+        if num_workers is None and \
+           batch_size is None and \
+           filterf is None and \
+           batcher is None:
+               g = self.func
+               self.func = lambda x: func(g(x))
+        else:
+            assert self.q_out is None
+            self.q_out = Queue(self.q_size)
+            return BufferedPipe(
+                    func, self, 
+                    q_in=self.q_out, 
+                    num_workers=num_workers if num_workers else self.num_workers, 
+                    batch_size=batch_size if batch_size else self.batch_size, 
+                    filterf=filterf if filterf else self.filterf,
+                    batcher=batcher if batcher else self.batcher)
+
+    def _wrapped_func(self, x):
+        if isinstance(x, tuple):
+            try:
+                return (x[0], self.func(x[1]))
+            except BaseException as e:
+                self.logger.warn('Applying function to {} raised exception {}'
+                        .format(x[1], e.message))
+                self.logger.exception(e)
+                return (x[0], None)
+
+        elif isinstance(x, list) and isinstance(x[0], tuple):
+            batch_index = [el[0] for el in x]
+            batch_input = self.batcher([el[1] for el in x])
+            try:
+                batch_output = self.func(batch_input)
+            except BaseException as e:
+                self.logger.warn('Applying function to {} raised exception {}'
+                        .format(batch_input, e.message))
+                self.logger.exception(e)
+                batch_output = [None] * len(batch_index)
+
+            return zip(batch_index, batch_output)
+
+        else:
+            try:
+                return self.func(x)
+            except BaseException as e:
+                self.logger.warn('Applying function to {} raised exception {}'
+                        .format(x, e.message))
+                self.logger.exception(e)
+                return None
+
+
+class ModelPipe:
+    def __init__(self):
+        self._pipe = BufferedPipe()
+
+    def add(self, func, num_workers=0, batch_size=1, filterf=lambda x: x is not None, batcher=lambda x:x):
+        self._pipe = self._pipe.add(func, num_workers, batch_size=batch_size, filterf=filterf, batcher=batcher)
+        return self
+
+    def apply_unordered(self, data):
+
+        if not isinstance(data, dict):
+            count_gen = itertools.count(start=0, step=1)
+            indexed_gen = itertools.izip(count_gen, (x for x in data))
+        else:
+            indexed_gen = ((k,v) for k,v in data.iteritems())
+            
+        output_gen = self._pipe(indexed_gen)
+
+        if isinstance(data, list):
+            return [x for x in output_gen]
+        elif isinstance(data, types.GeneratorType):
+            return output_gen
+        elif isinstance(data, dict):
+            return {x[0]:x[1] for x in output_gen}
+        elif isinstance(data, set):
+            return {x[1] for x in output_gen}
+
+    def apply_ordered(self, data):
+        unordered = self.apply_unordered(data)
+        if isinstance(data, dict) or \
+           isinstance(data, set):
+            return unordered
+        elif isinstance(data, list):
+            return [x[1] for x in sorted(unordered, key=lambda x:x[0])]
+        elif isinstance(data, types.GeneratorType):
+            return (x[1] for x in sorted(unordered, key=lambda x:x[0]))
+
+    def __call__(self, data):
+        return self.apply_unordered(data)
+
 
 
 class KerasModelWrapper:
@@ -53,9 +242,9 @@ class KerasModelWrapper:
             output_gen =  (self._wrapped_postprocessing(el) for el in inner_output)
 
             if isinstance(data, types.GeneratorType):
-                return filtered_output
+                return filterfed_output
             elif isinstance(data, types.ListType):
-                return [el for el in filtered_output]
+                return [el for el in filterfed_output]
             else:
                 return output_gen.iter().next()
 
@@ -87,6 +276,7 @@ class KerasModelWrapper:
             except BaseException as e:
                 self.logger.error('prediction on batch raised exception {}'
                         .format(e.message))
+                self.logger.exception(e)
                 return [None] * len(b)
 
 
@@ -143,7 +333,44 @@ class TensorFlowModelWrapper:
         raise NotImplementedError
 
 
-def _q2q_batch(func, queue_in, queue_out, filt=lambda x: True, batch_size=32, timeout=1):
+def resize_to_model_input(model, input_index=0):
+
+    input_shape = tuple([x.value for x in model.inputs[input_index].shape if x.value])
+    assert keras is not None
+    assert len(input_shape) == 3 
+
+    if len(input_shape) == 3:  
+        assert input_shape[0] == 1 or input_shape[2] == 1 or \
+               input_shape[0] == 3 or input_shape[2] == 3
+
+        if input_shape[0] == 1 or input_shape[0] == 3:
+            data_format = 'channels_first'
+        else:
+            data_format = 'channels_last'
+
+
+    def _run_resize(input_img):
+        #assert instanceof(image, Image)
+        if len(input_shape) == 3:
+            img = input_img.resize((input_shape[1], input_shape[0]), Image.ANTIALIAS)
+       
+        arr = image_prep.img_to_array(img, data_format)
+        arr /= 255.0
+
+        if input_shape[0] == 1 and arr.shape[0] > 1:
+            arr = np.mean(arr, axis=0)
+
+        if input_shape[2] == 1 and arr.shape[2] > 1:
+            arr = np.mean(arr, axis=2)
+
+        return arr.reshape((1,) + input_shape)
+    
+    return _run_resize
+
+
+
+
+def _q2q_batch(func, queue_in, queue_out, filterf=lambda x: True, batch_size=32, timeout=1):
     while True:
         try:
             batch = [queue_in.get(True, timeout)]
@@ -156,7 +383,7 @@ def _q2q_batch(func, queue_in, queue_out, filt=lambda x: True, batch_size=32, ti
             except Empty:
                 break
 
-        batch = [el for el in batch if filt(el)]
+        batch = [el for el in batch if filterf(el)]
 
         retval = func(batch)
                 
@@ -169,8 +396,8 @@ def _q2q_batch(func, queue_in, queue_out, filt=lambda x: True, batch_size=32, ti
                 except Full:
                     pass
 
-def _q2q_single(func, queue_in, queue_out, filt=lambda x: True, timeout=1):
-    _q2q_batch(lambda b: [func(x) for x in b], queue_in, queue_out, filt, 1, timeout) 
+def _q2q_single(func, queue_in, queue_out, filterf=lambda x: True, timeout=1):
+    _q2q_batch(lambda b: [func(x) for x in b], queue_in, queue_out, filterf, 1, timeout) 
 
 def _gen2q(data, queue):
     while True:
@@ -187,7 +414,7 @@ def _gen2q(data, queue):
             except Full:
                 pass
 
-def _q2gen(queue, timeout=5):
+def _q2gen(queue, timeout=1):
     while True:
         try:
             yield queue.get(True, timeout=timeout)
--- studio/tests/model_util_test.py
+++ studio/tests/model_util_test.py
@@ -1,10 +1,24 @@
 import unittest
 import numpy as np
+import urllib
+
+from PIL import Image
+from io import BytesIO
+
 import keras
-from studio import model_util
+from keras.layers import Dense, Conv2D, Flatten
+
+from keras.models import Sequential
+from keras.datasets import mnist
+from keras.utils import to_categorical
+
+from timeout_decorator import timeout
 from Queue import Queue, Full, Empty
 
+from studio import model_util
+
 class ModelUtilTest(unittest.TestCase):
+    _multiprocess_can_split = True
 
     def test_q2q_batch(self):
         data = range(10)
@@ -71,8 +85,8 @@ class ModelUtilTest(unittest.TestCase):
 
 
     def test_predict_generator(self):
-        model = keras.models.Sequential()
-        model.add(keras.layers.Dense(2, input_shape=(2,)))
+        model = Sequential()
+        model.add(Dense(2, input_shape=(2,)))
     
 
         #weights = [np.array([[2, 0], [0, 2]])]
@@ -93,13 +107,192 @@ class ModelUtilTest(unittest.TestCase):
             print(x)
         
         
+class BufferedPipeTest(unittest.TestCase):
+    def test_pipe_simple(self):
+        p = model_util.BufferedPipe() \
+            .add(lambda x: x+1) \
+            .add(lambda x: x*x) 
+
+        l = list(p((x for x in range(1,5))))
+
+        expected_l = [4, 9, 16, 25]
+
+        self.assertEquals(l, expected_l)
+        
+
+    def test_pipe_buffer(self):
+        p = model_util.BufferedPipe() \
+            .add(lambda x: x+1, num_workers=32) \
+            .add(lambda x: x*x) 
+
+        out_gen = p((x for x in range(1,5)))
+
+        expected_l = [4, 9, 16, 25]
+
+        self.assertEquals(list(out_gen), expected_l)
+
+
+
+class ModelPipeTest(unittest.TestCase):
+    _multiprocess_can_split = True
+
+    def test_model_pipe(self):
+
+        p = model_util.ModelPipe()
+        p.add(lambda x: x*x, num_workers=32)
+
+
+        input_dict = {x:x for x in range(10)}
+        output_dict = p(input_dict)
+
+        expected_dict = {k:v*v for k,v in input_dict.iteritems()}
+
+        self.assertEquals(expected_dict, output_dict)
+        
+    @timeout(10)
+    def test_model_pipe_long(self):
+
+        p = model_util.ModelPipe()
+        p.add(lambda x: x*x, num_workers=32)
+
+
+        input_dict = {x:x for x in range(10000)}
+        output_dict = p(input_dict)
+
+        expected_dict = {k:v*v for k,v in input_dict.iteritems()}
+
+        self.assertEquals(expected_dict, output_dict)
+
+    def test_model_pipe_ordered(self):
+
+        p = model_util.ModelPipe()
+        p.add(lambda x: x*x, num_workers=32)
+
+
+        input_list = range(10)
+        output_list = p.apply_ordered(input_list)
+
+        expected_list = [x*x for x in input_list]
+
+        self.assertEquals(expected_list, output_list)
+
+    def test_model_pipe_batch(self):
+
+        p = model_util.ModelPipe()
+        p.add(lambda b: [x*x for x in b], batch_size=4)
+
+
+        input_list = range(10)
+        output_list = p.apply_ordered(input_list)
+
+        expected_list = [x*x for x in input_list]
+
+        self.assertEquals(expected_list, output_list)
+
+    def test_model_pipe_exception(self):
+
+        p = model_util.ModelPipe()
+        p.add(lambda x: 1.0 / x, num_workers=4)
+
+
+        input_list = range(-10,10)
+        output_list = p.apply_ordered(input_list)
+
+        expected_list = [1.0 / x if x != 0 else None for x in input_list]
+
+        self.assertEquals(expected_list, output_list)
+
+
+
+    def test_model_pipe_keras(self):
+
+        model = keras.models.Sequential()
+        model.add(keras.layers.Flatten(input_shape=(1,28,28)))
+        model.add(keras.layers.Dense(128, activation='relu'))
+        model.add(keras.layers.Dense(10, activation='softmax'))
+
+        p = model_util.ModelPipe()
+        #p.add(lambda url: urllib.readurl(url), num_workers=32)
+        #p.add(lambda img_bytes: ImageIO(img_bytes))
+        #p.add(model_util.reshape_image_to_input(model))
+        
+        input_data = [np.random.random((1,1,28,28)) for _ in range(2)]
+
+        p.add(model.predict, batch_size=64, batcher=np.vstack)
+        #p.add(model.predict)
+        
+        expected_output = [model.predict(x.reshape((1,1,28,28))) for x in input_data] 
+        output = p.apply_ordered(input_data)
+        
+        self.assertTrue(np.isclose(np.array(output).flatten(), 
+                                   np.array(expected_output).flatten()).all())
+
+
+    
+    def test_model_pipe_mnist_urls(self): 
+
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+        x_train = x_train.reshape(60000, 28, 28, 1)
+        x_test = x_test.reshape(10000, 28, 28, 1)
+        x_train = x_train.astype('float32')
+        x_test = x_test.astype('float32')
+        x_train /= 255
+        x_test /= 255
+
+        y_train = to_categorical(y_train, 10)
+        y_test = to_categorical(y_test, 10)
+
+        model = Sequential()
+
+        #model.add(Conv2D(32, 5, activation='relu', input_shape=(28,28,1), data_format='channels_last'))
+        #model.add(Conv2D(64, 3, activation='relu'))
+        #model.add(Flatten())
+        model.add(Flatten(input_shape=(28,28,1)))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(128, activation='relu'))
+
+        model.add(Dense(10, activation='softmax'))
+
+        no_epochs=3
+        batch_size=32
+
+        model.compile(loss='categorical_crossentropy', optimizer='adam')
+        
+        
+        model.fit(
+            x_train, y_train, validation_data=(
+                x_test,
+                y_test),
+            epochs=no_epochs,
+            batch_size=batch_size)
+        
+
+
+        pipe = model_util.ModelPipe()
+               
+        pipe.add(lambda url: urllib.urlopen(url).read())
+        pipe.add(lambda img: Image.open(BytesIO(img)))
+        pipe.add(model_util.resize_to_model_input(model))
+        pipe.add(lambda t: model.predict(t), num_workers=1)
+        pipe.add(np.argmax)
+
+        url5 = 'http://blog.otoro.net/assets/20160401/png/mnist_output_10.png'
+        url2 = 'http://joshmontague.com/images/mnist-2.png'
+        urlb = 'http://joshmontague.com/images/mnist-3.png'
+
+        #import pdb
+        #pdb.set_trace()
+
+        #output = pipe({url5:url5, url2:url2, urlb:urlb})
+        output = pipe.apply_ordered([url5])
+
+        print output
+
 
         
 
 
-
-
-   
 
 if __name__ == "__main__":
     unittest.main()
