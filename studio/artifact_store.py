1badf7e359ff7a271e8cef0ac6e93fbd86494645
Peter Zhokhov <peter.zhokhov@sentient.ai>
2017-08-11 10:47:36 -0700

    s3 storage and test

--- studio/artifact_store.py
+++ studio/artifact_store.py
@@ -1,5 +1,6 @@
 from firebase_artifact_store import FirebaseArtifactStore
 from gcloud_artifact_store import GCloudArtifactStore
+from s3_artifact_store import S3ArtifactStore
 
 
 def get_artifact_store(config, blocking_auth=True, verbose=10):
@@ -8,5 +9,7 @@ def get_artifact_store(config, blocking_auth=True, verbose=10):
             config, blocking_auth=blocking_auth, verbose=verbose)
     elif config['type'].lower() == 'gcloud':
         return GCloudArtifactStore(config, verbose=verbose)
+    elif config['type'].lower() == 's3':
+        return S3ArtifactStore(config, verbose=verbose)
     else:
         raise ValueError('Unknown storage type: ' + config['type'])
---
+++ studio/s3_artifact_store.py
@@ -0,0 +1,52 @@
+import logging
+import time
+import calendar
+
+try:
+    import boto3
+except BaseException:
+    boto3 = None
+
+from tartifact_store import TartifactStore
+logging.basicConfig()
+
+
+class S3ArtifactStore(TartifactStore):
+    def __init__(self, config, verbose=10, measure_timestamp_diff=True):
+        self.logger = logging.getLogger('S3ArtifactStore')
+        self.logger.setLevel(verbose)
+        self.client = boto3.client('s3')
+
+        self.bucket = config['bucket']
+        buckets = self.client.list_buckets()
+
+        if not self.bucket in [b['Name'] for b in buckets['Buckets']]:
+            self.client.create_bucket(Bucket=self.bucket)
+
+        super(S3ArtifactStore, self).__init__(measure_timestamp_diff)
+
+    def _upload_file(self, key, local_path):
+        self.client.upload_file(local_path, self.bucket, key)
+
+    def _download_file(self, key, local_path):
+        self.client.download_file(self.bucket, key, local_path)
+
+    def _delete_file(self, key):
+        self.client.delete_object(Bucket=self.bucket, Key=key)
+
+    def _get_file_url(self, key):
+        return self.client.generate_presigned_url('get_object', 
+                Params={'Bucket': self.bucket, 'Key': key})
+
+    def _get_file_timestamp(self, key):
+        obj = boto3.resource('s3').Object(self.bucket, key)
+
+        time_updated = obj.last_modified
+        if time_updated:
+            timestamp = calendar.timegm(time_updated.timetuple())
+            return timestamp
+        else:
+            return None
+
+    def get_qualified_location(self, key):
+        return 's3://' + self.bucket + '/' + key
