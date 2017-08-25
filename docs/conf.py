7e861ea8cac6732fd95f66779ce4866472108f76
nieoh <steph@nieoh.com>
2017-08-24 23:35:36 -0700

    white letter logo and edits to conf

--- docs/conf.py
+++ docs/conf.py
@@ -82,11 +82,19 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 
+try:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+except ImportError:
+    pass
+
+
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
 html_theme_options = {
    'collapse_navigation': True,
    'display_version': True,
