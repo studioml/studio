43188f1df895851d0b3c117f3bbf5b0dcffdec81
Jason Liang <jasonzliang@utexas.edu>
2017-08-31 18:47:06 -0700

    fixed bugs

--- studio/tests/save_model.py
+++ studio/tests/save_model.py
@@ -7,5 +7,10 @@ import os
 model = Sequential()
 model.add(Dense(2, input_shape=(2,)))
 
-model.set_weights([[[2, 0], [0, 2]]])
+weights = model.get_weights()
+new_weights = []
+for weight in weights:
+  new_wights.append(weight + 1)
+
+model.set_weights(new_weights)
 model.save(os.path.join(fs_tracker.get_model_directory(), 'weights.h5'))
