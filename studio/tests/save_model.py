6e3b31698b9df75063ad1c2c4dd2356885bf9bc3
Jason Liang <jasonzliang@utexas.edu>
2017-08-31 18:55:19 -0700

    fixed bugs

--- studio/tests/save_model.py
+++ studio/tests/save_model.py
@@ -1,16 +1,20 @@
+import os
+import numpy as  np
+
 from keras.layers import Dense
 from keras.models import Sequential
 
 from studio import fs_tracker
-import os
 
 model = Sequential()
 model.add(Dense(2, input_shape=(2,)))
 
 weights = model.get_weights()
-new_weights = []
-for weight in weights:
-  new_weights.append(weight + 1)
+new_weights = [np.array([[2, 0], [0, 2]])]
+# print weights
+# new_weights = []
+# for weight in weights:
+#   new_weights.append(weight + 1)
 
 model.set_weights(new_weights)
 model.save(os.path.join(fs_tracker.get_model_directory(), 'weights.h5'))
