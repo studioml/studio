0e3cf20516906a7185c9249d9dea5e27bf46b915
Jason Liang <jasonzliang@utexas.edu>
2017-08-31 18:47:37 -0700

    fixed bugs

--- studio/tests/save_model.py
+++ studio/tests/save_model.py
@@ -10,7 +10,7 @@ model.add(Dense(2, input_shape=(2,)))
 weights = model.get_weights()
 new_weights = []
 for weight in weights:
-  new_wights.append(weight + 1)
+  new_weights.append(weight + 1)
 
 model.set_weights(new_weights)
 model.save(os.path.join(fs_tracker.get_model_directory(), 'weights.h5'))
