34ebaa16dc2e74344f7c315dd6354edfa22c185c
nieoh <steph@nieoh.com>
2017-09-18 17:02:13 -0700

    change dict comparison assertion

--- studio/tests/hyperparam_test.py
+++ studio/tests/hyperparam_test.py
@@ -36,8 +36,8 @@ class HyperparamTest(unittest.TestCase):
             {'a': 1, 'b': 4}, {'a': 2, 'b': 4}, {'a': 3, 'b': 4},
             {'a': 1, 'b': 5}, {'a': 2, 'b': 5}, {'a': 3, 'b': 5}]
 
-        self.assertEqual(sorted(h.convert_to_tuples(hyperparams)),
-                        sorted(expected_tuples))
+        self.assertIs(cmp(sorted(h.convert_to_tuples(hyperparams)),
+                            sorted(expected_tuples)), 0)
 
 if __name__ == '__main__':
     unittest.main()
