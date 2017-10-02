d1046e58ff9b4ff025d7d0c47fe6f5688a2f16f1
Peter Zhokhov <peter.zhokhov@sentient.ai>
2017-10-02 14:32:43 -0700

    fixed a typo in experiment.py

--- studio/experiment.py
+++ studio/experiment.py
@@ -46,7 +46,7 @@ class Experiment(object):
             'tb': {
                 'local': fs_tracker.get_tensorboard_dir(key),
                 'mutable': True
-            }
+            },
             '_metrics': {
                 'local': fs_tracker.get_artifact_cache('_metrics', key),
                 'mutable': True
