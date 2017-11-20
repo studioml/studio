062c9d161fa784454e69720825b564cd0993273b
Peter Zhokhov <peter.zhokhov@sentient.ai>
2017-11-20 10:14:53 -0800

    fixed version.distance == None problem in setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(fname):
 
 
 def local_scheme(version):
-    if version.distance > 0:
+    if version.distance and version.distance > 0:
         return '.post' + str(version.distance)
     else:
         return ''
