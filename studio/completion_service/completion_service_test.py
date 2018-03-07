1697baca27c79de6021fe5993d8fa1fe57bf4bc6
Peter Zhokhov <peter.zhokhov@sentient.ai>
2018-03-07 16:41:08 +0300

    autopep8

--- studio/completion_service/completion_service_test.py
+++ studio/completion_service/completion_service_test.py
@@ -82,7 +82,7 @@ class CompletionServiceTest(unittest.TestCase):
 
         files['url'] = _file_url
 
-        # TODO peterz enable passing aws credentials to google workers 
+        # TODO peterz enable passing aws credentials to google workers
         # if has_aws_credentials():
         #     files['s3'] = _file_s3
 
