39e4cd29e773fef4e271cce439e89c594118f633
Karl Mutch <karlmutch@gmail.com>
2018-05-30 10:16:46 -0700

    pass logger into the runner get_queue function to prevent None exceptions

--- studio/completion_service/completion_service.py
+++ studio/completion_service/completion_service.py
@@ -114,7 +114,7 @@ class CompletionService:
 
         self.queue = runner.get_queue(queue_name=queue, cloud=self.cloud,
                                       config=studio_config,
-                                      logger=self.log,
+                                      logger=self.logger,
                                       verbose=self.verbose_level)
 
         self.queue_name = self.queue.get_name()
