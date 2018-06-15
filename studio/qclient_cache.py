56a9228ef3a2c5d5e728ab83aea24d47f2b0306c
Karl Mutch <karlmutch@gmail.com>
2018-06-15 01:30:21 -0700

    sort out the var args passing

--- studio/qclient_cache.py
+++ studio/qclient_cache.py
@@ -31,7 +31,7 @@ def get_cached_queue(
             logger=logger,
             verbose=verbose)
     if close_after is not None and close_after.total_seconds() > 0:
-        thr = threading.Timer(interval=close_after.total_seconds(), function=purge_rmq, args=[], kwargs={"q": q, "logger": logger})
+        thr = threading.Timer(interval=close_after.total_seconds(), function=purge_rmq, kwargs={"q": q, "logger": logger})
         thr.setDaemon(True)
         thr.start()
 
